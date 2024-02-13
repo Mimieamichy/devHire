2cb994ceeb4b6bf08f4d8c3dafa09000c9411f2b
miracle ezeh <amichy07@gmail.com>
2024-02-13 22:26:43 +0100

    file-name-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-
 import SideBar from "./components/SideBar";
 import MainBarContent from "./components/MainBarContent";
-//import Favourites from "./components/Favourites";
+import Favourites from "./components/Favourites";
+import { Routes, Route } from "react-router-dom";
+import ContextFile from "./components/ContextFileApp";
+import React from "react";
 
 export default function App() {
   return (
     <div className="wrapper">
-      <SideBar />
-      <MainBarContent />
+      <ContextFile>
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<MainBarContent />} />
+          <Route path="/fahv" element={<Favourites />} />
+        </Routes>
+      </ContextFile>
     </div>
   );
 }
